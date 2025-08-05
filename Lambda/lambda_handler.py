041f688c1a1fded1cb73a2ea732101659f16966d
sunsembrace <moyasinkhan2000@gmail.com>
2025-08-05 01:24:46 +0100

    tiny tweak on lambda handler but on the hashes

--- Lambda/lambda_handler.py
+++ Lambda/lambda_handler.py
@@ -16,7 +16,7 @@ S3_PREFIX = 'earthquake-data/'
 
 
 def lambda_handler(event, context):
-    S3_BUCKET = os.environ['S3_BUCKET'] #moved inside to catch missing env vars better.
+    S3_BUCKET = os.environ['S3_BUCKET'] #moved inside to catch missing env vars better..
     url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 
     try: 
